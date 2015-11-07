e08fc34d174e14526567e230982a2e92cd91b54c
Richie Kirchofer <rgkirch@gmail.com>
2015-11-07 13:06:44 -0500

    added terminal regex

--- lexer.py
+++ lexer.py
@@ -6,7 +6,21 @@ except IndexError:
     print( "Error, no input file.", file=sys.stderr )
     sys.exit(1)
 
-terminals = 
-
-
-
+ASSERT = r"assert"
+ASSIGN = r"assign"
+ELSE = r"else"
+EQUALITY = r"=="
+GREATER = r">"
+ID = r"[a-za-z]([0-9a-zA-Z_])*"
+IF = r"if"
+LBRACE = r"{"
+LPAREN = r"}"
+NAT = r"0|[1-9]+"
+NOT = r"!"
+OR = r"||"
+PRINTNAT = r"printNat"
+RBRACE = r"}"
+READNAT = r"readNat"
+RPAREN = r")"
+SEMICOLON = r";"
+WHILE = r"while"
