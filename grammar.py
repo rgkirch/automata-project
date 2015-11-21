110feb48c1788ef4d9bcbdd4918da2675a14e53f
Richie Kirchofer <rgkirch@gmail.com>
2015-11-21 15:52:35 -0500

    grammar firsts, needs follows

--- grammar.py
+++ grammar.py
@@ -1,4 +1,16 @@
 from collections import OrderedDict
+import sys
+
+#terminals = ["+*$"]
+#rules = \
+#{
+#    "S":["E$"],
+#    "A":["+TA"],
+#    "E":["TA"],
+#    "B":["*FB"],
+#    "T":["FB"],
+#    "F":["(E)", "x"]
+#}
 
 class Grammar:
     def __init__(self):
@@ -44,6 +56,30 @@ class Grammar:
             self.addRule(ruleInput)
             ruleInput = input("> ").strip()
         return self
+
+    def first(self, productions):
+        # if epsilon, return false so parent frame moves on to next character(terminal/nonterminal)
+        if not productions:
+            # dunno if null or empty string - being optimistic and returning "", relies on logic of caller
+            return ""
+        else:
+            firsts = ""
+            for prod in productions:
+                for c in prod:
+                    print("char",c)
+                    # if c is terminal then return c
+                    if c in self.terminals:
+                        firsts.append(c)
+                    # c is nonterminal, as for first of c
+                    elif c in self.rules.keys():
+                        f = self.first(gram.rules[c])
+                        if f:
+                            firsts.append(f)
+                        # else continue, check for c as next char in string
+                    else:
+                        print("error, character {0} not found as terminal or nonterminal\n".format(c), file=sys.stderr)
+                else:
+                    return ""
 
     def __str__(self):
         rules = []
