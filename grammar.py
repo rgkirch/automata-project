bcc5bd4d0330e099f12806140855c075578f9d3c
Richie Kirchofer <rgkirch@gmail.com>
2015-11-21 16:41:24 -0500

    tested first

--- grammar.py
+++ grammar.py
@@ -2,7 +2,7 @@ from collections import OrderedDict
 import sys
 
 class Grammar:
-    terminals = ["+*$"]
+    terminals = "()+*x$"
     rules = \
     {
         "S":["E$"],
@@ -58,7 +58,7 @@ class Grammar:
 
     # nonterminal -> ["production", "production"]
     def first(self, productions):
-        """Accepts a list of strings, treats each string as a production and compiles a new string that holds all of the possible terminal characters. Returns empty string if nullable."""
+        """Accepts a list of strings, treats each string as a production and compiles a new string that holds all of the possible terminal characters. Returns empty string if not non nullable."""
         # if list is empty, [""] will return true, catch epsilon later
         if not productions:
             # dunno if null or empty string - being optimistic and returning "", relies on logic of caller
@@ -68,17 +68,23 @@ class Grammar:
             for prod in productions:
                 # won't run on "" epsilon empty string
                 for c in prod:
-                    print("char",c)
                     # if c is terminal then return c
                     if c in self.terminals:
-                        firsts.append(c)
+                        firsts += c
+                        # stop looping through single production, move onto next production if exists
+                        break
                     # c is nonterminal, as for first of c
                     elif c in self.rules.keys():
-                        firsts += self.first(self.rules[c])
-                        #break
+                        # if nothing returned, move on to next one
+                        f = self.first(self.rules[c])
+                        # if terminal returned, don't look at next terminals/nonterminals
+                        if f:
+                            firsts += f
+                            break
                         # else continue, check for c as next char in string
                     else:
                         print("error, character {0} not found as terminal or nonterminal\n".format(c), file=sys.stderr)
+                        sys.exit(-1)
             return firsts
 
     def __str__(self):
@@ -96,7 +102,8 @@ def prompt():
 if __name__ == '__main__':
     #prompt()   
     g = Grammar()#.buildGrammar()    
-    g.first(g.rules["S"])
+    for non in g.rules.keys():
+        print("first({0}) = {1}".format(non, g.first(g.rules[non])))
     #print(g)
     
     
