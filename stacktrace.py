ba8af076d281de350de99e213750ac2d58e6129e
cpmoline <corbin3@gmail.com>
2015-11-21 18:10:32 -0500

    added run_stacktrace and printtrace

--- stacktrace.py
+++ stacktrace.py
@@ -1,21 +1,68 @@
-# Given a string, and an LL(1) parse table, determine
-# if the string is in the given language
+import grammar
 
 # representation of LL(1) parse table, for testing purposes
-parseTable = { "E" : {"(" : "TY", "n" : "TY"},
+exampleParseTable = { "E" : {"(" : "TY", "n" : "TY"},
         "Y" : {"+" : "+TY", ")" : "", "$" : ""},
         "T" : {"(" : "FX", "n" : "FX"},
-        "X" : {"+" : "", "*" : "*FX", ")" : "", "$" : ""}
+        "X" : {"+" : "", "*" : "*FX", ")" : "", "$" : ""},
         "F" : {"(" : "(E)", "F" : "n"}}
 
-def run_stacktrace(instring):
+# Given a string, and an LL(1) parse table, determine
+# if the string is in the given language
+def run_stacktrace(grammar, inputstring):
     # check that terminals in input string are in grammar
-    for sym in instring:
-        if sym not in nonterminals:
-            print("error: '{sym}' not in grammar".format())
+    for sym in inputstring:
+        if sym not in grammar.terminals:
+            print("error: '{}' not in grammar".format(sym))
+            return
+    if inputstring[-1] != '$':
+        inputstring += '$'
     
     # create stack and push startSymbol
-    stack = [startSymbol]
+    stack = [grammar.startSymbol]
+    
+    currentsym = inputstring[0] # current symbol at front of input string
+    symindex = 1
+    steps = [] # track of the state of the stack and input string at each step
+    while stack and currentsym != '$':
+        # if currentsym == ToS pop sym of stack and advance one sym
+        if currentsym == stack[-1]:
+            stack.pop
+            nextsym = inputstring[1]
+            symindex += 1
+            steps.append((stack, inputstring[symindex:]))
+        else:
+            print([i for i in stack])
+            top = stack.pop()
+            try:
+                stack.extend(list(grammar.parseTable[top][currentsym]))
+                steps.append((stack, inputstring[symindex:]))
+            except IndexError:
+                print(steps + '\n')
+                print("No action defined for input symbol {}, when {} is at top of stack".format(
+                    currentsym, top))
+                break
+                
+# prints the stacktrace in a pretty readable way, data is a list of 2-tuples
+# corresponding (1) input, and (2) current stack, the default parameter delay
+# will cause a sleep for n seconds between printing steps
+def printtrace(steps, delay=0):
+    # print column headers
+    colhdrs = "{0:<{1}} | step # | stack".format("input", len(inputstring)-len(input))
+    print(colhdrs)
+    print("-" * len(colhdrs))
+    # print the rest of the data
 
-    while True:
-        # compare input symbol with `
+def prompt():
+    print("Determine if a string is in a given LL(1) grammar")
+
+if __name__ == "__main__":
+    prompt()
+    g = grammar.Grammar()
+    g.parseTable = exampleParseTable
+    g.startSymbol = 'E'
+    g.terminals = ['+', '*', '(', ')', 'n', '$']
+    inputstring = input("Enter a string to check (empty string to quit): ")
+    while inputstring:
+       run_stacktrace(g, inputstring)
+       inputstring = input("Enter a string to check (empty string to quit): ")
